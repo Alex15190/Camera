e693c1580b8d91d3889beb03d35e1f0474d8946b
Alex Chekodanov <checkodanov@gmail.com>
2018-09-10 18:47:05 +0300

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Alex Chekodanov on 10.09.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Camera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Camera
+//
+//  Created by Alex Chekodanov on 10.09.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
